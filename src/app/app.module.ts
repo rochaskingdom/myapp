1c47267ee9baafca4007ed6cb8bf260e8fa090f1
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2021-01-08 20:59:40 -0300

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './myfirst.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyFirstComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'myfirst',
+    templateUrl: './myfirst.component.html'
+})
+export class MyFirstComponent {
+
+}
